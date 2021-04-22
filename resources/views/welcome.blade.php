ee0f728fa28f6af60e097ea134b7f09359b28385
MichalWolny <mwolny98@interia.pl>
2021-04-22 22:18:56 +0200

    little fixes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta author="Michał Wolny i Patryk Tomaszewski">
     <link href="/fonts/stylesheet.css" rel="stylesheet">
     <script src="https://www.google.com/recaptcha/api.js?onload=vueRecaptchaApiLoaded&render=explicit" async defer>
     </script>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
 
-    <title>{{env('APP_NAME')}}</title>
+    <title>Car Reservation System - Skorzystaj z naszych usług!</title>
 </head>
 <body>
     <div id="app">
